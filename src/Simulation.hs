5c59dd771acc3a3cae5fcb46a0a8ed93fab262b8
Edil Guimarães <j.edil@ene.unb.br>
2021-10-06 17:55:05 -0300

    Abstract executeSimulation type signature

--- src/Simulation.hs
+++ src/Simulation.hs
@@ -6,7 +6,8 @@ import Signal
 
 type Source = Signal Input
 
-executeSimulation :: Circuit Input Output -> Source -> [Time] -> [(Time, Output)]
+--executeSimulation :: Circuit Input Output -> Source -> [Time] -> [(Time, Output)]
+executeSimulation :: Circuit a b -> Signal a -> [Time] -> [(Time, b)]
 executeSimulation circuit input samples = do time <- samples
                                              let result = (circuit `simulate` input) `at` time
                                              return (time, result)                                           

ecaccd60bc994e2520ddcb0bb394c944952c387a
Eduardo Lemos <dudulr10@gmail.com>
2021-11-09 23:09:58 -0300

    Removed graphic support + generate output file with points + added python script

---
+++ scripts/graphicsIteration.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+xs = [0]
+ys = [0]
+with open("output.txt") as file:
+    for line in file:
+        l = line.split(' ')
+        xs.append(float(l[0]))
+        ys.append(float(l[1]))
+
+plt.plot(xs, ys)
+plt.axis([0, 9, -65, -45])
+plt.xlabel('Iterations')
+plt.ylabel('Output')
+plt.title('Iterations x Output')
+plt.savefig('output.png')
--- src/Utils.hs
+++ src/Utils.hs
@@ -2,38 +2,24 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Utils where
-import Lucid
-import Lucid.Html5
-import Graphics.Plotly
-import Graphics.Plotly.Lucid
-import Lens.Micro
 import Control.Monad.Fix
-import qualified Data.Text.Lazy as T
-import qualified Data.Text.Lazy.IO as T
+import System.IO
 import Signal
 import Circuit
 import AmpOp
 
 type Iteration = Int
 
-makePlot =
-    T.writeFile "output.html" $ renderText $ doctypehtml_ $ do
-    head_ $ do meta_ [charset_ "utf-8"]
-               plotlyCDN
-    body_ $ toHtml $ plotly "myDiv" [myTrace] 
-
-myTrace
-  = line (aes & x .~ fst
-              & y .~ snd) (ampOpInvertingList' lm741 12 r1 r2)
+makePlotIterations = do
+    outh <- openFile "output.txt" WriteMode
+    mapM_ (\(i, o) -> hPutStrLn outh (show i ++ " " ++ show o)) (ampOpInvertingList' lm741 12 r1 r2)
+    hClose outh
 
 ampOpInvertingList' :: AmpOp -> Input -> Resistor -> Resistor -> [(Iteration, Output)]
-ampOpInvertingList' model input r1 r2 = fix calculate (0, 100, [], 0)
+ampOpInvertingList' model input r1 r2 = fix calculate (0, 100, [], 1)
     where calculate f (vOutOld, vXOld, l, it) = if abs(vOutOld - vOut) <= 0.000001 && abs(vXOld - vX) <= 0.000001 then l ++ [(it, vOut)] else f (vOut, vX, l ++ [(it, vOut)], it + 1)
             where vX = (- vOutOld) / openLoopGain model
                   vOut = (vXOld * (r1 + r2) - (input * r2)) / r1
 
-checkData :: IO ()
-checkData = print (ampOpInvertingList' lm741 12 r1 r2)
-
---writeGraphFile :: IO ()
---writeGraphFile = Bin.encodeFile "output.txt" $ ampOpInvertingList' lm741 12 r1 r2
+checkDataIterations :: IO ()
+checkDataIterations = print (ampOpInvertingList' lm741 12 r1 r2)

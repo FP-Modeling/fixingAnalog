058d2449fedfc5d039695ae3a374bc94b9602065
Eduardo Lemos <dudulr10@gmail.com>
2021-11-10 02:27:39 -0300

    Fixed graphs + input signals + scripts

--- src/Utils.hs
+++ src/Utils.hs
@@ -25,14 +25,20 @@ ampOpInvertingList' model input r1 r2 = fix calculate (100, 0, [], 1)
 checkDataIterations :: IO ()
 checkDataIterations = print (ampOpInvertingList' lm741 12 r1 r2)
 
-makePlotTime = do
+makePlotTimeOutput = do
     outh <- openFile "outputTime.txt" WriteMode
-    mapM_ (\(t, i, o) -> hPutStrLn outh (show t ++ " " ++ show i ++ " " ++ show o)) (concat $ makeSamples outlet [1..100.0])
+    mapM_ (\(t, i, o) -> hPutStrLn outh (show t ++ " " ++ show i ++ " " ++ show o)) (concat $ makeSamplesOutput sen [0.1,0.2..10.0])
     hClose outh
+
+makePlotTimeInput = do
+    outh <- openFile "inputTime.txt" WriteMode
+    mapM_ (\(t, o) -> hPutStrLn outh (show t ++ " " ++ show o)) (makeSamplesInput sen [0.1,0.2..10.0])
+    hClose outh
+
 
 ampOpInvertingList :: AmpOp -> Resistor -> Resistor -> Circuit Input [(Iteration, Output)]
 ampOpInvertingList model r1 r2 =
-    ($ (dc10000000, ground, [], 1))<$> mfix
+    ($ (dc100, ground, [], 1))<$> mfix
     (\f -> Circuit $ \vIn -> Signal $ \time (vXOld, vOutOld, l, it) ->
         let vX = (/ openLoopGain model) <$> (- vOutOld)
             vOut = (/ r1) <$> (((* (r1 + r2)) <$> vXOld) - ((* r2) <$> vIn))
@@ -40,13 +46,18 @@ ampOpInvertingList model r1 r2 =
             then l ++ [(it, vOut `at` time)]
             else f (vX, vOut, l ++ [(it, vOut `at` time)], it + 1))
 
-makeSamples :: Signal Input -> [Time] -> [[(Time, Iteration, Output)]]
-makeSamples input samples = do time <- samples
-                               let results = (ampOpInvertingList lm741 r1 r2 `simulate` input) `at` time
-                               mapM (\(i, o) -> [(time, i, o)]) results
+makeSamplesInput :: Signal Input -> [Time] -> [(Time, Input)]
+makeSamplesInput input samples = do time <- samples
+                                    let result = input `at` time
+                                    return (time, result)
+
+makeSamplesOutput :: Signal Input -> [Time] -> [[(Time, Iteration, Output)]]
+makeSamplesOutput input samples = do time <- samples
+                                     let results = (ampOpInvertingList lm741 r1 r2 `simulate` input) `at` time
+                                     mapM (\(i, o) -> [(time, i, o)]) results
 
 checkDataTime :: [(Iteration, Output)]
-checkDataTime = do (i, o) <- (ampOpInvertingList lm741 r1 r2 `simulate` outlet) `at` 1
+checkDataTime = do (i, o) <- (ampOpInvertingList lm741 r1 r2 `simulate` sen) `at` 1
                    return (i,o)
 
 -- checkDataTime :: IO ()

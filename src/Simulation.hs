a35972d623988fd178292ac7ab12ead4d2189519
Eduardo Lemos <dudulr10@gmail.com>
2021-11-17 23:56:54 -0300

    Fixed computation of values in monadic approach

--- src/Simulation.hs
+++ src/Simulation.hs
@@ -4,10 +4,21 @@ import AmpOp
 import Circuit
 import Signal
 
-type Source = Signal Input
+executeSimulation :: SignalState -> (SignalState -> Either String (Circuit a SignalState)) -> Signal a -> [Time] -> Either String [(Time, Output)]
+executeSimulation _ _ _ [] = Right []
+executeSimulation initialState buildCircuit input (t:ts) = buildCircuit initialState >>= \circuit -> do let result = circuit `simulate` input `at` t
+                                                                                                        rest <- executeSimulation result buildCircuit input ts
+                                                                                                        Right $ (t, getSignalOutput result `at` t) : rest
+                              
+iSignalState = SignalState2 (dc100, ground)
+c1 = ampOpInverting lm741 r1 r2
 
---executeSimulation :: Circuit Input Output -> Source -> [Time] -> [(Time, Output)]
-executeSimulation :: Circuit a b -> Signal a -> [Time] -> [(Time, b)]
-executeSimulation circuit input samples = do time <- samples
-                                             let result = (circuit `simulate` input) `at` time
-                                             return (time, result)                                           
+simulationSignal = executeSimulation iSignalState c1 sen [1..100]
+
+iState = State2 (100, 0)
+c2 = ampOpInverting' lm741 r1 r2 12
+
+simulation = executeSimulation' iState c2
+
+executeSimulation' :: State -> (State -> Either String Output) -> Either String Output
+executeSimulation' initialState buildCircuit = buildCircuit initialState >>= Right
